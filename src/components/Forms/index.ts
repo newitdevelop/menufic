17e70b8a60c05839fbe3aba214cd7f4ec17e0024
newitdevelop <desenvolvimento@newalliance-it.com>
2025-12-29 02:43:20 +0000

    try modal fix v2

--- src/components/Forms/index.ts
+++ src/components/Forms/index.ts
@@ -2,4 +2,5 @@ export { BannerForm } from "./BannerForm";
 export { CategoryForm } from "./CategoryForm";
 export { MenuForm } from "./MenuForm";
 export { MenuItemForm } from "./MenuItemForm";
+export { PackForm } from "./PackForm";
 export { RestaurantForm } from "./RestaurantForm";
