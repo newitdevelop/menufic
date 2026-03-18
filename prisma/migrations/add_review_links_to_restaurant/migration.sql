17e25c427babcdab86dd9137e6044d9cb1194a21
newitdevelop <desenvolvimento@newalliance-it.com>
2026-03-18 21:58:38 +0000

    fixes

--- prisma/migrations/add_review_links_to_restaurant/migration.sql
+++
@@ -1,3 +0,0 @@
--- Add Google Place ID and TripAdvisor URL to Restaurant
-ALTER TABLE "Restaurant" ADD COLUMN IF NOT EXISTS "googlePlaceId" TEXT;
-ALTER TABLE "Restaurant" ADD COLUMN IF NOT EXISTS "tripadvisorUrl" TEXT;
